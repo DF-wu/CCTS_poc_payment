a68628b26060f5a6ae89b667c95023268265d2fd
dfder <a2470abc@gmail.com>
2022-02-23 21:10:19 +0800

    feat: standardize the entity.

--- src/main/java/tw/dfder/ccts_poc_payment/Entity/PaymentMessageEnvelope.java
+++ src/main/java/tw/dfder/ccts_poc_payment/Entity/PaymentMessageEnvelope.java
@@ -1,25 +1,41 @@
 package tw.dfder.ccts_poc_payment.Entity;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Document(collection = "PaymentMessageEnvelope")
 public class PaymentMessageEnvelope {
-    String paymentID;
-    String buyerID;
+    @Id
+    private String Id;
+
+    // "get", "pay"
+    @Field
+    String method;
+    @Field
+    String paymentId;
+    @Field
+    String buyerId;
+    @Field
     int totalAmount;
+    @Field
     boolean isValid;
 
-    public String getPaymentID() {
-        return paymentID;
+
+    public String getPaymentId() {
+        return paymentId;
     }
 
-    public void setPaymentID(String paymentID) {
-        this.paymentID = paymentID;
+    public void setPaymentId(String paymentId) {
+        this.paymentId = paymentId;
     }
 
-    public String getBuyerID() {
-        return buyerID;
+    public String getBuyerId() {
+        return buyerId;
     }
 
-    public void setBuyerID(String buyerID) {
-        this.buyerID = buyerID;
+    public void setBuyerId(String buyerId) {
+        this.buyerId = buyerId;
     }
 
     public int getTotalAmount() {
@@ -36,5 +52,22 @@ public class PaymentMessageEnvelope {
 
     public void setValid(boolean valid) {
         isValid = valid;
+    };
+
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
     }
 }
