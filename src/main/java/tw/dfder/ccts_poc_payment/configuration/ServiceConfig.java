bbcc29031929105614be679caa3233cb4dcfdd8e
dfder <a2470abc@gmail.com>
2022-03-17 20:27:48 +0800

    fix: inject spring property as POJO.

--- src/main/java/tw/dfder/ccts_poc_payment/configuration/ServiceConfig.java
+++ src/main/java/tw/dfder/ccts_poc_payment/configuration/ServiceConfig.java
@@ -1,25 +1,39 @@
 package tw.dfder.ccts_poc_payment.configuration;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.Map;
 
 /**
  * To load application.yml setting
  * @Author df
- * @version v1
+ * @version v2
  */
-@Configuration
+@Component
+@ConfigurationProperties(prefix = "serviceinfo")
 public class ServiceConfig {
-    @Value("${serviceInfo.name}")
     public String serviceName;
 //
 //    @Value("${serviceInfo.pact}")
 //    public static String correspondingPact;
 
-    @Value("${serviceInfo.destinations}")
     public List<String> destinations;
 
+
+    public String getServiceName() {
+        return serviceName;
+    }
+
+    public void setServiceName(String serviceName) {
+        this.serviceName = serviceName;
+    }
+
+    public List<String> getDestinations() {
+        return destinations;
+    }
+
+    public void setDestinations(List<String> destinations) {
+        this.destinations = destinations;
+    }
 }
