f48837447f2a78e0bbb0eb48ceac2124ad8adb85
df <df@dfder.tw>
2022-03-29 18:44:59 +0800

    fix: reading application.yaml .

--- src/main/java/tw/dfder/ccts_poc_payment/configuration/ServiceConfig.java
+++ src/main/java/tw/dfder/ccts_poc_payment/configuration/ServiceConfig.java
@@ -1,6 +1,8 @@
 package tw.dfder.ccts_poc_payment.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -10,16 +12,16 @@ import java.util.List;
  * @Author df
  * @version v2
  */
-@Component
+@Configuration
 @ConfigurationProperties(prefix = "serviceinfo")
 public class ServiceConfig {
+
     public String serviceName;
 //
 //    @Value("${serviceInfo.pact}")
 //    public static String correspondingPact;
 
     public List<String> destinations;
-
 
     public String getServiceName() {
         return serviceName;
