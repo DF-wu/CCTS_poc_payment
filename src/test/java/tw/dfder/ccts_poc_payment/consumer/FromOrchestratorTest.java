ccc09abcfbe70b536f8a8ecdb0d8f4c57ed7f165
df <df@dfder.tw>
2022-03-30 23:58:52 +0800

    feat: add contract test case.

---
+++ src/test/java/tw/dfder/ccts_poc_payment/consumer/FromOrchestratorTest.java
@@ -0,0 +1,48 @@
+package tw.dfder.ccts_poc_payment.consumer;
+
+
+import au.com.dius.pact.consumer.MessagePactBuilder;
+import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
+import au.com.dius.pact.consumer.junit5.PactTestFor;
+import au.com.dius.pact.consumer.junit5.ProviderType;
+import au.com.dius.pact.core.model.annotations.Pact;
+import au.com.dius.pact.core.model.messaging.Message;
+import au.com.dius.pact.core.model.messaging.MessagePact;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@ExtendWith(PactConsumerTestExt.class)
+@PactTestFor(providerName = "orchestrator", providerType = ProviderType.ASYNCH)
+public class FromOrchestratorTest {
+
+    @Pact(consumer = "paymentService")
+    public MessagePact validateMessageFromOrchestra(MessagePactBuilder builder) {
+        return builder
+                .expectsToReceive("response payment")
+                .withMetadata(m -> {
+                    m.add("source", "pointService");
+                    m.add("destination", "orchestrator");
+                })
+                .toPact();
+
+    }
+
+    @Test
+    @PactTestFor(pactMethod = "validateReceivedFromPointServiceMessageBuilder")
+    public void validateReceivedFromPointServiceMessageBuilderTest(List<Message> messages) {
+
+        // 起碼有上面的案例吧
+        assertThat(messages).isNotEmpty();
+        // 驗header
+        messages.forEach(m -> {
+            assertThat(m.getMetadata()).hasFieldOrProperty("source");
+            assertThat(m.getMetadata()).hasFieldOrProperty("destination");
+        });
+
+    }
+}
---
+++ src/test/java/tw/dfder/ccts_poc_payment/provider/ToOrchestratorTest.java
@@ -0,0 +1,60 @@
+package tw.dfder.ccts_poc_payment.provider;
+
+
+import au.com.dius.pact.core.model.Interaction;
+import au.com.dius.pact.core.model.Pact;
+import au.com.dius.pact.provider.MessageAndMetadata;
+import au.com.dius.pact.provider.PactVerifyProvider;
+import au.com.dius.pact.provider.junit5.MessageTestTarget;
+import au.com.dius.pact.provider.junit5.PactVerificationContext;
+import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
+import au.com.dius.pact.provider.junitsupport.Consumer;
+import au.com.dius.pact.provider.junitsupport.Provider;
+import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
+import com.google.gson.Gson;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
+import tw.dfder.ccts_poc_payment.Entity.PaymentMessageEnvelope;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+@Provider("paymentService")
+@Consumer("orchestrator")
+@PactBroker(url = "http://23.dfder.tw:10141")
+public class ToOrchestratorTest {
+
+    @TestTemplate
+    @ExtendWith(PactVerificationInvocationContextProvider.class)
+    void testTemplate(Pact pact, Interaction interaction, PactVerificationContext context) {
+        context.verifyInteraction();
+    }
+
+
+    @BeforeEach
+    void before(PactVerificationContext context) {
+        context.setTarget(new MessageTestTarget());
+        System.setProperty("pact.verifier.publishResults", "true");
+        System.setProperty("pact.provider.version", "v0.1");
+    }
+
+
+    @PactVerifyProvider("update point message")
+    public MessageAndMetadata verifyMessageOfPayment() {
+
+        Gson gson = new Gson();
+        PaymentMessageEnvelope msg = new PaymentMessageEnvelope();
+        msg.setPaymentId(UUID.randomUUID().toString());
+        msg.setBuyerId(UUID.randomUUID().toString());
+        msg.setTotalAmount((int) (Math.random()*1000));
+        msg.setValid(true);
+        msg.setMethod("response");
+
+        HashMap<String, String> props = new HashMap<>();
+        props.put("source", "orchestrator");
+        props.put("destination","pointService");
+        return new MessageAndMetadata(gson.toJson(msg).getBytes(), props);
+    }
+
+}
