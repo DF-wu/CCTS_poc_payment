450e27c9e7675070982e412e22029a8269f07fdb
df <df@dfder.tw>
2022-04-14 01:14:24 +0800

    fix: update contract.

--- src/test/java/tw/dfder/ccts_poc_payment/provider/ToOrchestratorTest.java
+++ src/test/java/tw/dfder/ccts_poc_payment/provider/ToOrchestratorTest.java
@@ -36,7 +36,7 @@ public class ToOrchestratorTest {
     void before(PactVerificationContext context) {
         context.setTarget(new MessageTestTarget());
         System.setProperty("pact.verifier.publishResults", "true");
-        System.setProperty("pact.provider.version", "v0.1");
+        System.setProperty("pact.provider.version", "v0.2");
     }
 
 
