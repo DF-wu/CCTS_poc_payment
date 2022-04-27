e0a2f6baac639e4be94486fda84c319d2d2ffef2
df <df@dfder.tw>
2022-04-27 21:51:11 +0800

    feat: update contract.

--- publish-contract.sh
+++ publish-contract.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run --rm -w ${PWD} -v ${PWD}:${PWD} pactfoundation/pact-cli:latest publish ${PWD}/Contracts/  --broker-base-url http://23.dfder.tw:10141  --consumer-app-version "v0.2" --tag-with-git-branch
+docker run --rm -w ${PWD} -v ${PWD}:${PWD} pactfoundation/pact-cli:latest publish ${PWD}/Contracts/  --broker-base-url http://23.dfder.tw:10141  --consumer-app-version "v0.3" --tag-with-git-branch
 
